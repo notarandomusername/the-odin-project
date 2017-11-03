a7c357e31925100e218e317cc51e6164000da6a0
notarandomusername <33181909+notarandomusername@users.noreply.github.com>
2017-11-03 11:41:07 -0400

    Changed the size of trueLen to match container.

--- ColorGrid/application.js
+++ ColorGrid/application.js
@@ -1,5 +1,5 @@
 function LoadDivs(n) {
-    var trueLen = Math.floor(960/n);
+    var trueLen = Math.floor(720/n);
     for (var i = 0; i < n; i++) {
       for (var j = 0; j < n; j++) {
           $(".container").append("<div class='unit'></div>");
